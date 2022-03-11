50b70c22dddedfa45ba9696555f821b1f47fcf26
nupur507 -git <nupurverma730@gmail.com>
2022-03-12 00:00:33 +0530

    post perfect run

--- app.js
+++ app.js
@@ -1,53 +1,31 @@
-const fs = require('fs');
 const express = require('express');
+const morgan = require('morgan');
+
+const tourRouter = require('./routes/tourRoutes');
+const userRouter = require('./routes/userRoutes');
 
 const app = express();
 
-app.use(express.json());
-// app.get('/', (res, req)=>
-// {
-// res.status(200).send('hello from the server side! ');
-// });
-
-const tours = JSON.parse(
-    fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
-);
-
-app.get('/api/v1/tours' , (req, res) =>
-{
-  res.status(200).json({
-      status: 'success',
-      results: tours.length,
-            data:
-      {
-          tours
-      }
-
-  });
-});
-
-app.post('/api/v1/tours' , (req, res) =>
-{
-//   console.log(req.body);
-  
-const newId = tours(tours.length - 1). id + 1;
-const newTour = Object.assign({ id: newId }, req.body);
-
-tours.push(newTour);
-fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, 
-JSON.stringify(tours), 
-err =>
-{
-res.status(201).json({
-status: 'success',
-data:{
-    tour: newTour
+// 1) MIDDLEWARES
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
 }
-});
-});
+
+app.use(express.json());
+app.use(express.static(`${__dirname}/public`));
+
+app.use((req, res, next) => {
+  console.log('Hello from the middleware 👋');
+  next();
 });
 
-const port = 3000;
-app.listen(port , () => {
-console.log(`App running on  ${port}...`);
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
 });
+
+// 3) ROUTES
+app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/users', userRouter);
+
+module.exports = app;
