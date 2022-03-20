d1ff80b409c0a61a5caf319e526d00dd85a8e6e4
nupur507 -git <nupurverma730@gmail.com>
2022-03-20 19:30:48 +0530

    database connect success

--- server.js
+++ server.js
@@ -1,22 +1,24 @@
-
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-
-dotenv.config({ path: './config.env' });
 const app = require('./app');
 
+dotenv.config({ path: './config.env' });
+ 
 const DB = process.env.DATABASE.replace(
-  '<PASSWORD>',
-  process.env.DATABASE_PASSWORD
-);
+ '<PASSWORD>',
+    process.env.DATABASE_PASSWORD);
 
-mongoose
-  .connect(DB, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false
+mongoose.connect(DB,  
+  {
+useNewUrlParser: true,
+useCreateIndex: true,
+useFindAndModify: false
+
   })
-  .then(() => console.log('DB connection successful!'));
+  .then(con=>{
+      console.log(con.connections);
+      console.log('DB connection sucessfully');
+    });
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
